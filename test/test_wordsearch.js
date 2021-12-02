18e7dd1159ca1dfdbe39e46e81f4f527810f615d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-02 00:53:47 +0000

    complete design of wordsearch fn

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,92 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if word is found vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'Y', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'EYE')
+    assert.isTrue(result);
+  })
+  it("should retun false if word is not found vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'X', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'EYE');
+    assert.isFalse(result);
+  });
+
+  it('should return false if given letters matrix is an empty array', () => {
+    const result = wordSearch([], "sword");
+    assert.isFalse(result);
+  });
+
+  it("should return true if word is found vertically in a matrix that originally contained empty arrays", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'Y', 'A', 'L'],
+      [],
+      ['H', 'M', 'J', 'T', 'E', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'EYE')
+    assert.isTrue(result);
+
+
+  });
+
+  // LEAVING FOR STRETCH WORK IF WE HAVE TIME AFTER CORE WORK
+  // it("should return true is word is found horizontally where the given word has a mix of upper and lower case characters", function() {
+  //   const result = wordSearch([
+  //     ['A', 'w', 'C', 'f', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'F', 'c', 'f', 'Q', 'y', 'A', 'L'],
+  //     ['H', 'M', 'J', 'T', 'E', 'E', 'R', 'G'],
+  //     ['W', 'n', 'O', 's', 'E', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'NoSe')
+  //   assert.isTrue(result);
+  // });
+
+  // it("should return true is word is found vertically where the given word has a mix of upper and lower case characters", function() {
+  //   const result = wordSearch([
+  //     ['A', 'w', 'C', 'f', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'E', 'I', 'n', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'F', 'c', 'o', 'Q', 'y', 'A', 'L'],
+  //     ['H', 'M', 'J', 'S', 'E', 'E', 'R', 'G'],
+  //     ['W', 'n', 'O', 'E', 'E', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'NoSe')
+  //   assert.isTrue(result);
+  // });
+
+
+
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,77 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+/*
+// this will ensure horizontal search works properly
+ return false after both horizontal search and vertical search
+
+//another case: letters is empty at beginning
+// if statement at beginning that checks if letters is an empty array
+// if true, return false
+
+// vertical search:
+// call transpose on our letters, store that in a variable
+// horizontally join transposed array
+// conduct same search using for loop as we did for horizontal search
+// return true if a given row includes our given word argument
+
+at very end return false (search failure)
+*/
+// const transpose = require('/vagrant/w2/d2/transpose'); // REMOVED FOR SUBMISSION PURPOSES AND PASTED TRANSPOSE FN WITHIN CURRENT FILE
+
+const transpose = function(matrix) {
+  /*
+
+  Initialize the result matrix as an empty array
+  Iterate in the given matrix length using nested for loop
+  Outer loop will iterate in the rows of the given matrix
+  Inner loop will iterate in the columns of the given matrix - will reference to the length of the row
+  If statement in the inner loop = if row is equal to 0 - push the given matrix [row][col] into the result matrix as an array
+  else:
+  push the value in matrix at matrix[row][col] into newMatrix[col]
+  return result matrix
+  
+  */
+
+  let newMatrix = [];
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let column = 0; column < matrix[row].length; column++) {
+      if (row === 0) {
+        newMatrix.push([matrix[row][column]]);
+      } else {
+        newMatrix[column].push(matrix[row][column]);
+      }
+    }
+  }
+
+  return newMatrix;
+
+};
+
+
+const wordSearch = (letters, word) => {
+
+
+    const horizontalJoin = letters.map(ls => ls.join('')) // joins letters in each row into a string
+    // horizontal search:
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    const transposedArr = transpose(letters)
+    const horizontalJoin2 = transposedArr.map(ls => ls.join(""))
+    // vertical search:
+    for (v of horizontalJoin2) {
+        if (v.includes(word)) return true
+    }
+
+    return false
 }
 
 module.exports = wordSearch
+
+// for visualization
+
+// console.log(wordSearch([
+//     ['d', 'o', 'g'],
+//     ['c', 'a', 't'],
+//     ['r', 'a', 't']
+// ]))
